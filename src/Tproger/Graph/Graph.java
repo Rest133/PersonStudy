3369dbb72f82d21ca7459614f5fe61e2b4981a19
Rest133 <dubrovinanatolii@mail.ru>
2018-12-05 00:42:53 +0300

    DFS and BFS

--- src/Tproger/Graph/Graph.java
+++ src/Tproger/Graph/Graph.java
@@ -1,5 +1,8 @@
 package Tproger.Graph;
 
+import java.util.*;
+import java.util.concurrent.DelayQueue;
+
 public class Graph {
     private final int MAX_VERTS = 20;
     private Vertex vertexList[];
@@ -29,4 +32,57 @@ public class Graph {
     public void displayVertex(int v) {
         System.out.println(vertexList[v].label);
     }
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int j = 0; j < nVerts; j++) {
+            if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
+                return j;
+            }
+        }
+        return -1;//смежных вершин не осталось или их просто нет
+    }
+
+    public void dfs() {
+        vertexList[0].wasVisited = true;//Пометка
+        displayVertex(0);
+        Stack theStack = new Stack();
+        theStack.push(0);
+
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex((int) theStack.peek());
+            if (v == -1) theStack.pop();
+            else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
+        }
+        for (int j = 0; j < nVerts; j++) {
+            vertexList[j].wasVisited = false;
+        }
+    }
+
+    public void bfs() // Обход в ширину
+    { // Алгоритм начинает с вершины 0
+        vertexList[0].wasVisited = true; // Пометка
+        displayVertex(0); // Вывод
+        Queue theQueue = new Queue();
+        theQueue.insert(0); // Вставка в конец очереди
+        int v2;
+        while (!theQueue.isEmpty()) // Пока очередь не опустеет
+        {
+            int v1 = theQueue.remove(); // Извлечение вершины в начале очереди
+// Пока остаются непосещенные соседи
+            while ((v2 = getAdjUnvisitedVertex(v1)) != -1) { // Получение вершины
+                vertexList[v2].wasVisited = true; // Пометка
+                displayVertex(v2); // Вывод
+                theQueue.insert(v2); // Вставка
+            }
+        }
+// Очередь пуста, обход закончен
+        for (int j = 0; j < nVerts; j++) // Сброс флагов
+            vertexList[j].wasVisited = false;
+    }
+
+
 }
